8c24ab14f30ba42c7c76f911e021473b4f651691
WyattMac <wyatt6@163.com>
2025-08-13 10:48:27 +0800

    Create UserOrganizationAttached.java

---
+++ src/main/java/fun/xianlai/admax/module/iam/model/entity/UserOrganizationAttached.java
@@ -0,0 +1,27 @@
+package fun.xianlai.admax.module.iam.model.entity;
+
+import fun.xianlai.admax.module.iam.model.entity.pk.UserOrganizationPK;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Wyatt6
+ * @date 2025/8/13
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_iam_user_organization_attached")
+@IdClass(UserOrganizationPK.class)
+public class UserOrganizationAttached {
+    @Id
+    private Long userId;
+    @Id
+    private Long organizationId;
+}
