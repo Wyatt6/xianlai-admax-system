1b4f97fe1964db2a3a1b417a6c74f3c8ab990735
WyattMac <wyatt6@163.com>
2025-08-15 08:57:10 +0800

    Update UserController.java

--- src/main/java/fun/xianlai/admax/module/iam/controller/UserController.java
+++ src/main/java/fun/xianlai/admax/module/iam/controller/UserController.java
@@ -58,37 +58,48 @@ public class UserController {
     @ControllerLog("登录API")
     @PostMapping("/login")
     public RetResult login(@RequestBody User input) {
-            Assert.notNull(input, "输入数据为空");
-            Assert.hasText(input.getCaptchaKey(), "验证码KEY为空");
-            Assert.hasText(input.getCaptcha(), "验证码为空");
-            Assert.hasText(input.getUsername(), "用户名为空");
-            Assert.hasText(input.getPassword(), "密码为空");
+        Assert.notNull(input, "输入数据为空");
+        Assert.hasText(input.getCaptchaKey(), "验证码KEY为空");
+        Assert.hasText(input.getCaptcha(), "验证码为空");
+        Assert.hasText(input.getUsername(), "用户名为空");
+        Assert.hasText(input.getPassword(), "密码为空");
 
-            String captchaKey = input.getCaptchaKey().trim();
-            String captcha = input.getCaptcha().trim();
-            String username = input.getUsername();
-            String password = input.getPassword();
-            log.info("请求参数: captchaKey=[{}], captcha=[{}], username=[{}]", captchaKey, captcha, username);
+        String captchaKey = input.getCaptchaKey().trim();
+        String captcha = input.getCaptcha().trim();
+        String username = input.getUsername();
+        String password = input.getPassword();
+        log.info("请求参数: captchaKey=[{}], captcha=[{}], username=[{}]", captchaKey, captcha, username);
 
-            captchaService.verifyCaptcha(captchaKey, captcha);
-            if (!userService.checkUsernameFormat(username)) {
-                throw new SystemException("用户名格式错误");
-            }
-            if (!userService.checkPasswordFormat(password)) {
-                throw new SystemException("密码格式错误");
-            }
+        captchaService.verifyCaptcha(captchaKey, captcha);
+        if (!userService.checkUsernameFormat(username)) {
+            throw new SystemException("用户名格式错误");
+        }
+        if (!userService.checkPasswordFormat(password)) {
+            throw new SystemException("密码格式错误");
+        }
 
-            User user = userService.authentication(username, password);
-            log.info("登录：Sa-Token框架自动生成token并缓存");
-            StpUtil.login(user.getId());
-            log.info("loginId=[{}]", StpUtil.getLoginId());
-            log.info("token=[{}]", StpUtil.getTokenValue());
-            log.info("sessionId=[{}]", StpUtil.getSession().getId());
+        User user = userService.authentication(username, password);
+        log.info("登录：Sa-Token框架自动生成token并缓存");
+        StpUtil.login(user.getId());
+        log.info("loginId=[{}]", StpUtil.getLoginId());
+        log.info("token=[{}]", StpUtil.getTokenValue());
+        log.info("sessionId=[{}]", StpUtil.getSession().getId());
 
-            // TODO 登录日志
+        // TODO 登录日志
 
-            return new RetResult().success()
-                    .addData("token", StpUtil.getTokenValue())
-                    .addData("tokenExpiredTime", System.currentTimeMillis() + StpUtil.getTokenTimeout() * 1000);
+        return new RetResult().success()
+                .addData("token", StpUtil.getTokenValue())
+                .addData("tokenExpiredTime", System.currentTimeMillis() + StpUtil.getTokenTimeout() * 1000);
+    }
+
+    @ControllerLog("重置密码API")
+    @PostMapping("/resetPassword")
+    public RetResult resetPassword(@RequestBody User input) {
+        Assert.notNull(input, "输入数据为空");
+        Assert.hasText(input.getCaptchaKey(), "验证码KEY为空");
+        Assert.hasText(input.getCaptcha(), "验证码为空");
+        Assert.hasText(input.getUsername(), "用户名为空");
+        Assert.hasText(input.getPassword(), "密码为空");
+        return null;
     }
 }
