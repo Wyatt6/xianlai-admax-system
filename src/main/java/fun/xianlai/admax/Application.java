4c1f3d45bea6f631c76de7691888b3fdf7892b4b
WyattMac <wyatt6@163.com>
2025-08-16 21:05:16 +0800

    修改日志

--- src/main/java/fun/xianlai/admax/Application.java
+++ src/main/java/fun/xianlai/admax/Application.java
@@ -12,6 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        log.info("--------------- ONCE应用已启动 ---------------");
+        log.info("-------------------- XianLai - Admax 系统已成功启动 --------------------");
     }
 }
