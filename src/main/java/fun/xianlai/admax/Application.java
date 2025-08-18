8173feaf19a0fe14e7143d5abb319a2380a0ab32
WyattMac <wyatt6@163.com>
2025-08-18 15:27:24 +0800

    创建工程

---
+++ src/main/java/fun/xianlai/admax/Application.java
@@ -0,0 +1,11 @@
+package fun.xianlai.admax;
+
+/**
+ * @author WyattLau
+ */
+public class Application {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
