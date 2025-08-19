9c9e8216ae4733527064370cdc4b15d1fbaaacc6
WyattMac <wyatt6@163.com>
2025-08-19 14:19:35 +0800

    新建异常类

---
+++ src/main/java/fun/xianlai/admax/exception/SystemException.java
@@ -0,0 +1,18 @@
+package fun.xianlai.admax.exception;
+
+/**
+ * @author WyattLau
+ */
+public class SystemException extends RuntimeException {
+    public SystemException() {
+        super("系统错误");
+    }
+
+    public SystemException(String message) {
+        super(message);
+    }
+
+    public SystemException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
