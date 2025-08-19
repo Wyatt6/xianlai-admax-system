50c2c2e52fe5b99d38ff5fc8e31600be29205873
WyattMac <wyatt6@163.com>
2025-08-20 00:23:39 +0800

    添加Service方法的日志打印注解

---
+++ src/main/java/fun/xianlai/admax/loggers/SimpleServiceLog.java
@@ -0,0 +1,15 @@
+package fun.xianlai.admax.loggers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author WyattLau
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SimpleServiceLog {
+    String value() default "";
+}
