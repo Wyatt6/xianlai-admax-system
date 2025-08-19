3da870283d5b629b286d2420377d4495fa4d3d63
WyattMac <wyatt6@163.com>
2025-08-19 09:45:27 +0800

    新建主键ID生成器

---
+++ src/main/java/fun/xianlai/admax/supports/PrimaryKeyGenerator.java
@@ -0,0 +1,31 @@
+package fun.xianlai.admax.supports;
+
+import fun.xianlai.admax.utils.FakeSnowflakeUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.generator.EventType;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.io.Serializable;
+
+/**
+ * 主键ID生成器
+ * 使用FakeSnowflakeUtil中定义的伪雪花算法
+ *
+ * @author WyattLau
+ */
+public class PrimaryKeyGenerator implements IdentifierGenerator {
+    synchronized public long next() {
+        return FakeSnowflakeUtil.nextId();
+    }
+
+    @Override
+    public Object generate(SharedSessionContractImplementor session, Object owner, Object currentValue, EventType eventType) {
+        return IdentifierGenerator.super.generate(session, owner, currentValue, eventType);
+    }
+
+    @Override
+    public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+        return next();
+    }
+}
