5d404a634f067f22d34580a27060728d9a43c348
WyattMac <wyatt6@163.com>
2025-08-19 09:51:12 +0800

    新建Map对象和数据库JSON类型转换器

---
+++ src/main/java/fun/xianlai/admax/supports/MapAndJsonConverter.java
@@ -0,0 +1,36 @@
+package fun.xianlai.admax.supports;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.Map;
+
+/**
+ * JPA数据类型转换器：Java Map和JDBC JSON互转
+ *
+ * @author WyattLau
+ */
+@Converter
+public class MapAndJsonConverter implements AttributeConverter<Map<String, Object>, String> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("无法将Map对象转换为数据库JSON类型", e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String dbData) {
+        try {
+            return objectMapper.readValue(dbData, Map.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("无法将数据库JSON类型读取为Map对象", e);
+        }
+    }
+}
