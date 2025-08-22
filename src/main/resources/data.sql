498e35eea8f8ef64b42474614d0e72b5784126fd
WyattMac <wyatt6@163.com>
2025-08-22 11:50:40 +0800

    优化系统参数实体

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO tb_system_system_option(`active`, `built_in`, `editable`, `sort_id`, `tag`, `option_key`, `option_value`, `name`, `description`) VALUES (1, 1, 1, 0, 'sys', 'sys.captcha.length', '5', '验证码长度', '推荐4～6位');
-INSERT IGNORE INTO tb_system_system_option(`active`, `built_in`, `editable`, `sort_id`, `tag`, `option_key`, `option_value`, `name`, `description`) VALUES (1, 1, 1, 0, 'sys', 'sys.captcha.expireSeconds', '60', '验证码有效期', '单位：秒');
+INSERT IGNORE INTO tb_system_system_option(`active`, `built_in`, `editable`, `front_load`, `sort_id`, `option_key`, `option_value`, `name`, `description`) VALUES (1, 1, 1, 1, 0, 'sys.captcha.length', '5', '验证码长度', '推荐4～6位');
+INSERT IGNORE INTO tb_system_system_option(`active`, `built_in`, `editable`, `front_load`, `sort_id`, `option_key`, `option_value`, `name`, `description`) VALUES (1, 1, 1, 0, 0, 'sys.captcha.expireSeconds', '60', '验证码有效期', '单位：秒');
