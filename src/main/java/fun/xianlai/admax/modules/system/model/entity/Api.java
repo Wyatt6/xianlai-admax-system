41d8b1ce264be69fa10b4b9e428607441a23eccf
WyattMac <wyatt6@163.com>
2025-08-27 09:43:45 +0800

    修改注释内容

--- src/main/java/fun/xianlai/admax/modules/system/model/entity/Api.java
+++ src/main/java/fun/xianlai/admax/modules/system/model/entity/Api.java
@@ -20,6 +20,8 @@ import org.hibernate.annotations.GenericGenerator;
 
 /**
  * 系统接口
+ *  - 内置接口：已在Admax中定义，接口数据由程序SQL自动创建，不允许修改
+ *  - 非内置接口：由开发者在二次开发中定义，接口数据由具有权限的用户通过管理台新增和管理
  *
  * @author WyattLau
  */
@@ -59,7 +61,7 @@ public class Api {
     private String tag;                 // 打标分类
 
     @Column(columnDefinition = "bit not null default 0")
-    private Boolean builtIn;            // 内置参数（仅允许修改optionValue和sortId）
+    private Boolean builtIn;            // 内置参数
 
     @Column(columnDefinition = "bit not null default 0")
     private Boolean active;
