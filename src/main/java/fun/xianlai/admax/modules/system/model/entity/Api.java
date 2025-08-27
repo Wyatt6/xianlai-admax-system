9bc00c57154616c5287daeee06507ec7e1f8ac12
WyattMac <wyatt6@163.com>
2025-08-27 08:49:05 +0800

    创建系统请求实体类

---
+++ src/main/java/fun/xianlai/admax/modules/system/model/entity/Api.java
@@ -0,0 +1,66 @@
+package fun.xianlai.admax.modules.system.model.entity;
+
+import fun.xianlai.admax.modules.system.model.enums.RequestMethod;
+import fun.xianlai.admax.supports.PrimaryKeyGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * 系统接口
+ *
+ * @author WyattLau
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "tb_system_api", indexes = {
+        @Index(columnList = "url", unique = true),
+        @Index(columnList = "sortId")
+})
+public class Api {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "PK_generator")
+    @GenericGenerator(name = "PK_generator", type = PrimaryKeyGenerator.class)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(10) not null default 'get'")
+    private RequestMethod requestMethod;
+
+    @Column(columnDefinition = "varchar(1024) not null")
+    private String url;
+
+    @Column(length = 1024)
+    private String description;
+
+    @Column(length = 14000)
+    private String requestSpec;         // 请求规范
+
+    @Column(length = 14000)
+    private String responseSpec;        // 响应规范
+
+    @Column(columnDefinition = "bit not null default 0")
+    private Boolean builtIn;            // 内置参数（仅允许修改optionValue和sortId）
+
+    @Column(columnDefinition = "bit not null default 0")
+    private Boolean active;
+
+    @Column(columnDefinition = "bigint not null default 0")
+    private Long sortId;
+}
---
+++ src/main/java/fun/xianlai/admax/modules/system/model/enums/RequestMethod.java
@@ -0,0 +1,9 @@
+package fun.xianlai.admax.modules.system.model.enums;
+
+/**
+ * @author WyattLau
+ */
+public enum RequestMethod {
+    get,
+    post
+}
