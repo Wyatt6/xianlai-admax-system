808e51c7ae0dd8f719674ae2bca2c7b8c78ed90d
WyattMac <wyatt6@163.com>
2025-08-27 16:06:37 +0800

    修改系统参数初始数据

--- src/main/resources/data-options.sql
+++ src/main/resources/data-options.sql
@@ -1,5 +1,8 @@
 INSERT IGNORE INTO tb_system_option(`active`, `built_in`, `editable`, `front_load`, `sort_id`, `option_key`, `option_value`, `name`, `description`)
-VALUES (1, 1, 1, 1, 0, 'sys.captcha.length', '5', '验证码长度', '推荐4～6位');
+VALUES (1, 1, 1, 1, 0, 'console.openLog', 'false', '控制台日志打印开关', 'true/false，默认：false');
 
 INSERT IGNORE INTO tb_system_option(`active`, `built_in`, `editable`, `front_load`, `sort_id`, `option_key`, `option_value`, `name`, `description`)
-VALUES (1, 1, 1, 0, 0, 'sys.captcha.expireSeconds', '60', '验证码有效期', '单位：秒');
+VALUES (1, 1, 1, 1, 0, 'captcha.length', '5', '验证码长度', '推荐4～6位，默认：5');
+
+INSERT IGNORE INTO tb_system_option(`active`, `built_in`, `editable`, `front_load`, `sort_id`, `option_key`, `option_value`, `name`, `description`)
+VALUES (1, 1, 1, 0, 0, 'captcha.expireSeconds', '60', '验证码有效期', '单位：秒，默认：60');
