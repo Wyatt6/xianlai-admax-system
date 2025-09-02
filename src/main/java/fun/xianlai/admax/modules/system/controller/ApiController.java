99d5385e1db1251a47accaaae8ccd8169bcd6b7d
WyattMac <wyatt6@163.com>
2025-09-02 23:06:36 +0800

    修改名词

--- src/main/java/fun/xianlai/admax/modules/system/controller/ApiController.java
+++ src/main/java/fun/xianlai/admax/modules/system/controller/ApiController.java
@@ -23,14 +23,14 @@ public class ApiController {
     private ApiService apiService;
 
     // TODO 添加访问权限
-    @ControllerLog("更新系统接口缓存")
+    @ControllerLog("更新接口缓存")
     @GetMapping("/updateApisCache")
     public RetResult updateApisCache() {
         apiService.updateApisCache();
         return new RetResult().success();
     }
 
-    @ControllerLog("获取系统接口")
+    @ControllerLog("获取接口")
     @GetMapping("/getApis")
     public RetResult getApis() {
         List<Map<String, Object>> apis = apiService.getApis();
